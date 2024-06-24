bc219f00582fac02604df83ed952ba12dc64a015
함동균 <dh4m28@gmail.com>
2024-06-24 16:38:14 +0900

    api

--- apc_front/src/firebase/FirebaseInstance.js
+++ apc_front/src/firebase/FirebaseInstance.js
@@ -4,6 +4,9 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth, updatePassword } from "firebase/auth"; 
 import { getStorage, ref as storageRef, uploadBytes, getDownloadURL } from "firebase/storage";
 import { getDatabase, set, push, ref as databaseRef, get, update, runTransaction, remove, child } from 'firebase/database';
+import dotenv from 'dotenv';
+
+dotenv.config({path: '../../.env'});
 
 // Todo: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -11,7 +14,14 @@ import { getDatabase, set, push, ref as databaseRef, get, update, runTransaction
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  
+    apiKey: API_KEY,
+    authDomain: AUTH_DOMAIN,
+    projectId: PROJECT_ID,
+    storageBucket: STORAGE_BUCKET,
+    messagingSenderId: MESSAGING_SENDER_ID,
+    databaseURL: DATABASE_URL,
+    appId: APP_ID,
+    measurementId: MEASUREMENT_ID
 };
 
 // Initialize Firebase
