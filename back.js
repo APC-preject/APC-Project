f9b597cbcfb9249d7a359494007cb9c2203300e9
함동균 <dh4m28@gmail.com>
2024-06-25 17:35:59 +0900

    ProductDetailPage

--- back.js
+++ back.js
@@ -15,6 +15,7 @@ const port = 4000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/orders/:id', async (req, res) => {
   const { id } = req.params;
@@ -151,6 +152,83 @@ app.post('/productImages/:name', upload.single('image'), async (req, res) => {
   } catch (error) {
     console.error('Error uploading image:', error);
     res.status(500).json({ message: 'Error uploading image', error });
+  }
+});
+
+app.get('/products/:productId', async (req, res) => {
+  const productId = req.params.productId;
+  const productRef = db.ref(`products/${productId}`);
+  try {
+    const snapshot = await productRef.get();
+    if (snapshot.exists()) {
+      res.status(200).json(snapshot.val());
+    } else {
+      res.status(404).json({ message: 'Product not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching Product', error });
+  }
+});
+
+app.get('/reviews/:productId', async (req, res) => {
+  const productId = req.params.productId;
+  const reviewRef = db.ref(`reviews/${productId}`);
+  try {
+    const snapshot = await reviewRef.get();
+    if (snapshot.exists()) {
+      res.status(200).json(snapshot.val());
+    } else {
+      res.status(404).json({ message: 'review not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching review', error });
+  }
+});
+
+app.post('/orders', async (req, res) => {
+  const {newOrder, newDeliveryWait, id, productId, quantity, provider_Name, orderedPrice} = req.body;
+  const orderRef = db.ref(`orders/${id}`);
+  try {
+    const productRef = db.ref(`products/${productId}`);
+
+    await productRef.transaction((product) => {
+      if (product) {
+        if (product.pQuantity >= quantity) {
+          product.pQuantity -= quantity;
+        } else {
+          throw new Error('주문 수량이 재고 수량보다 많습니다.');
+        }
+      }
+      return product;
+    });
+
+    try {
+      // 주문 정보 db에 저장
+      const newOrderRef = orderRef.push();
+
+      const orderID = newOrderRef.key;
+
+      const updates = {};
+      updates[`orders/${id}/${orderID}`] = newOrder;
+      updates[`deliveryWaits/${provider_Name}/${productId}/${orderID}`] = newDeliveryWait;
+      // 주문 관련 데이터 동시 업데이트 (원자성 만족)
+      await db.ref().update(updates);
+
+      res.status(200).json({ message: `총 ${quantity}kg 주문하셨습니다. 가격은 ${orderedPrice}원 입니다.` });
+    } catch (error) {
+      // 주문 데이터 저장 중 문제 생겼으니 뺏던 수량 원상 복구
+      await productRef.transaction((product) => {
+        if (product) {
+          product.pQuantity += quantity;
+        }
+        return product;
+      });
+      console.log(error);
+      res.status(500).json({ message: '주문 데이터 저장 중 문제가 발생했습니다.', error });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: '수량 확인 및 업데이트 중 문제가 발생했습니다.', error });
   }
 });
 
