538574c635e9c958faca1647b376067a7cab80e2
함동균 <dh4m28@gmail.com>
2024-06-25 09:46:25 +0900

    orderManagementDetail

--- back.js
+++ back.js
@@ -30,6 +30,60 @@ app.get('/orders/:id', async (req, res) => {
   }
 });
 
+app.get('/orders/:id/:orderid', async (req, res) => {
+  const { id } = req.params;
+  const orderRef = db.ref(`orders/${id}/${orderid}`);
+  try {
+    const snapshot = await get(orderRef);
+    if (snapshot.exists()) {
+      res.status(200).json(snapshot.val());
+    } else {
+      res.status(404).json({ message: 'Order not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching order', error });
+  }
+});
+
+app.post('ordersGo/:id/:orderid', async (req, res) => {
+  const { id, orderid } = req.params;
+  const { trackingNumber, productId } = req.body;
+  const orderRef = db.ref();
+  try {
+    if (trackingNumber === '') {
+      res.status(400).json({ message: 'Tracking number is required' });
+      return;
+    }
+    const departedDate = new Date().toLocaleString();
+    const updates = {}
+    updates[`orders/${id}/${orderid}/departedDate`] = departedDate
+    updates[`orders/${id}/${orderid}/deliveryStatus`] = 1
+    updates[`deliveryWaits/${id}/${productId}/${orderid}/deliveryStatus`] = 1 // id 와 userId 동일한 값인지 확인
+    updates[`orders/${id}/${orderid}/trackingNum`] = trackingNumber
+    await orderRef.update(updates);
+    res.status(200).json({ message: 'Departed' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error updating order', error });
+  }
+});
+
+app.post('ordersArrive/:id/:orderid', async (req, res) => {
+  const { id, orderid } = req.params;
+  const { productId } = req.body;
+  const orderRef = db.ref();
+  try {
+    const arrivedDate = new Date().toLocaleString();
+    const updates = {}
+    updates[`orders/${id}/${orderid}/arrivedDate`] = arrivedDate
+    updates[`orders/${id}/${orderid}/deliveryStatus`] = 2
+    updates[`deliveryWaits/${id}/${productId}/${orderid}`] = null
+    await orderRef.update(updates);
+    res.status(200).json({ message: 'Departed' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error updating order', error });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
